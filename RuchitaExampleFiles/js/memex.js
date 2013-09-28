b48f2a849912a9e985fca4aeb174a3ce738c0c23
ruchita20 <ruchita.rathi@gmail.com>
2013-09-27 21:01:13 -0700

    js file

--- RuchitaExampleFiles/js/memex.js
+++ RuchitaExampleFiles/js/memex.js
@@ -16,15 +16,19 @@ var url_name;
 /*-------------End of Var Definition-----------------*/
 
  $(document).on('submit', "#load_bookmarks", function () {
-        var username = $("#tag_request_username").val();
-
+        var username = $("#username").val();
+        
+        var string_list = [];
         getAllTags_APICall(username).done(function (data) {
-            var string_list = [];
             $.each(data, function (i, val) {
-                string_list.push(i + "("+val+")");
+/*            $( ".span9" ).append( '<p>'+'<a href="getURLs(username,'+ i+');" id="tag">'+i+'</a>'+'('+val+')'+'</p>');
+*/           
+$('.span9 .bookmarks p').append('  <span id="add_here">'+i+'('+val+')</span>');
             });
             
-            printList("#Elements",string_list);
+            
+            
+            
         });
 
         return false;
@@ -48,33 +52,10 @@ delicious = getSuggestedTags(username, url_name);
 console.log(delicious);
 });
 
-/*function print entries*/
-function printEntries(body_div,entries) {
-    var title;
-    var url;
-    var description;
-    var tags;
-    var html_injection = '<table class="entry_format" border="1">';
-    var iframe;
-    $(body_div).html("");
-    //for(var i=0; i < entries.length; i++){
-    //    url = entries[i].u;
-    //    $(body_div).append('<li>' + url + '</li>');
-    //}	
-    //console.log( entries);
-    jQuery.each(entries, function (i, val) {       
-        html_injection = '<table class="entry_format" border="1">';
-        iframe = '<div class="box"> <iframe src=' + val.u + ' width = "500px" height = "500px"></iframe> </div>';
-        url = "<tr>" + '<td>URL: </td> <td> <a href="' + val.u + '">' + val.u +"</a>"+"</td></tr>";
-        title = "<tr>" + "<td>Title: </td> <td>" + val.d + "</td></tr>";
-        description = "<tr>" + "<td>Notes: </td> <td>" + val.n + "</td></tr>";
-        tags = "<tr>" + "<td>Tags: </td> <td>" + val.t + "</td></tr>";
-        html_injection = html_injection + title + url + description +tags+ "</table>";        
-        $(body_div).append("<li>" + html_injection + "</li>");
-        //console.log(html_injection);               
-    });    
-}
-/*end of print entries function*/
+/*Helper functions*/
+/*Display all tags */
+
+
   });
   
   
